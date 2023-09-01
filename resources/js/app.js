189f975ac03e9376c23247713c3cc1a0f19bfd30
krystiano13 <crystianotv@gmail.com>
2023-09-01 19:53:12 +0200

    fix bug which causes action to set to undefined

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,4 @@
 import "./bootstrap";
-import { Form } from "./form";
 
 let type;
 
@@ -10,27 +9,23 @@ const main = document.querySelector("main");
 
 form.classList.add("hidden");
 
-cancelButton.addEventListener('click', (e) => {
+cancelButton.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log('test');
+    console.log("test");
     form.classList.add("hidden");
 });
 
 main.addEventListener("click", (e) => {
-    console.log(e.target);
-
     if (e.target.className === "edBtn btn btn-warning") {
         type = "edit";
         formButton.innerHTML = "Edit";
         form.classList.remove("hidden");
+        form.action = `/edit/${e.target.id}`
     } else if (e.target.id === "add") {
         type = "add";
         formButton.innerHTML = "Add";
         form.classList.remove("hidden");
+        form.action = '/add/'
     }
 
-    if (form && formButton && cancelButton) {
-        const formInstance = new Form(type, formButton);
-        form.action = formInstance.createForm();
-    }
 });
